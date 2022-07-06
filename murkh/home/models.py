ae16a0ebe3fea38de464ec3fd623abb7dbee877e
kartik31002 <95395681+kartik31002@users.noreply.github.com>
2022-07-06 17:02:11 +0530

    Add files via upload

--- murkh/home/models.py
+++ murkh/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class movies(models.Model):
+    movieid = models.IntegerField()
+    title = models.CharField(max_length=1000)
+    overview = models.CharField(max_length=1000)
+    rating = models.FloatField()
+    release = models.DateField()
+    
--- murkh/home/views.py
+++ murkh/home/views.py
@@ -1,5 +1,21 @@
+import json
 from django.shortcuts import render, HttpResponse
+import requests
+
+apiBaseURL = 'https://api.themoviedb.org/3/'
+apiKey = '3d9b4ef8cf0ea93474001f18dabe2e00'
+
+imageBaseUrl = 'https://image.tmdb.org/t/p/'
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    response = requests.get(apiBaseURL + 'movie/now_playing?api_key=' + apiKey).json()
+    # popular_dict = json.loads(response)
+    popular_movies = response["results"]
+    movie_id = []
+    for i in popular_movies:
+        movie_id.append(i["id"])
+    response2 = []
+    for i in movie_id:
+        response2.append(requests.get(apiBaseURL + 'movie/' + str(i) + '?api_key=' + apiKey).json())
+    return render(request, 'index.html', response2)
